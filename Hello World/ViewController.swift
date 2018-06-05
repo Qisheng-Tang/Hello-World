a85aa1cd8fc443a0dfbcfcc88c73dea98152b050
唐琪盛 <tangqisheng@mwc-024214.dhcp.missouri.edu>
2018-06-05 15:31:44 -0500

    First Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by 唐琪盛 on 6/5/18.
+//  Copyright © 2018 Qisheng Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
